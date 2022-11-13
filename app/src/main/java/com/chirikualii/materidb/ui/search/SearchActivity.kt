2ce311974297e528188c88dc2100448b86a4b03f
DESKTOP-UN3B90A\Lab-ERP-16 <putrianggunzubaidah@gmail.com>
2022-11-13 16:47:08 +0700

    first commit

---
+++ app/src/main/java/com/chirikualii/materidb/ui/search/SearchActivity.kt
@@ -0,0 +1,51 @@
+package com.chirikualii.materidb.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.core.widget.doOnTextChanged
+import com.chirikualii.materidb.R
+import com.chirikualii.materidb.data.model.Movie
+import com.chirikualii.materidb.databinding.ActivitySearchBinding
+import com.chirikualii.materidb.ui.adapter.MovieListAdapter
+
+class SearchActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchBinding
+    private lateinit var listAdapter : MovieListAdapter
+
+    val viewModel: SearchViewModel by viewModels<SearchViewModel>(
+        factoryProducer = { SearchViewModelFactory(context = this) }
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        observeView()
+        listAdapter = MovieListAdapter()
+        binding.rvMovie.adapter = listAdapter
+
+
+        binding.etSearch.doOnTextChanged { text, _, _, _ ->
+            Toast.makeText(this, "Searching ${text}", Toast.LENGTH_SHORT).show()
+            viewModel.doSearchMovie(text.toString())
+        }
+    }
+
+    private fun observeView() {
+        viewModel.listMovie.observe(this){
+            listAdapter.addItem(it)
+        }
+
+        viewModel.isLoading.observe(this){
+            if(it) {
+                binding.progressBar.visibility = View.VISIBLE
+            }else{
+                binding.progressBar.visibility = View.INVISIBLE
+            }
+        }
+    }
+}
